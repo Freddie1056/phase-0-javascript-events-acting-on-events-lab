e0b8dfa6a72dc1edf395980ca36ce212777ea90e
Fredrick <freddyfred976@gmail.com>
2023-06-23 04:05:52 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ dodger.style.backgroundColor = "#FF69B4"
 
 
 function moveDodgerLeft() {
-    const leftNumbers = dodger.style.left.replace("px", "");
-    const left = parseInt(leftNumbers, 10);
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
     
     if (left > 0) {
         dodger.style.left = `${left - 1}px`;
